1286282de842562f256b8d9ffc02449cabfc6f7b
yosuke1985 <ihatenonoildressing@gmail.com>
2018-11-07 11:08:37 +0900

    refactor

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -55,32 +55,15 @@ class ProductController extends Controller
      */
     public function pageShow($page)
     {
-
-
-//        $pageNumberList = range(($page * 10)-9, $page * 10);
-
         $products = Product::orderBy('updated_at', 'desc')->select('id','name','price','description','created_at', 'updated_at')->take(10)->get();
 
 
         // 型の調べ方
         \Log::debug(get_class($products));
 
-
-//        $filtered = $products->filter(function ($product) {
-//            \Log::debug(get_class(collect($product)));
-//
-//
-//                collect($product).forget('created_at');
-//
-//
-//        });
-
         $filtered = $products->map(function ($item) {
 
-//            \Log::debug(collect($item).forget('created_at'));
 //            \Log::debug(get_class(collect($item)));
-//            collect($item).forget('created_at');
-//            \Log::debug(collect($item)->forget('created_at'));
             $item = collect($item)->forget('created_at');
             \Log::debug($item);
 
@@ -90,14 +73,8 @@ class ProductController extends Controller
 
         return response()->json($filtered);
 
-
         //User tableとArticleテーブルを作って記事にユーザーを登録
         //ユーザーごとの記事一覧
-
-
-
-
-
 
     }
 
